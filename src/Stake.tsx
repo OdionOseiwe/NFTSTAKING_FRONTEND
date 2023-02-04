079d9ee1131b418bc68df55f55ff7966947483a4
OdionOseiwe <oseiweifebhor@gmail.com>
2023-02-04 11:12:09 +0100

    writing

--- src/Stake.tsx
+++ src/Stake.tsx
@@ -25,9 +25,6 @@ export default function Stake() {
   return (
     <div className='text-orange-50 bg-zinc-900 p-8 min-h-screen'>
       <div className='sm:flex justify-between p-10 '>
-        <div className='md:text-3xl lg:text-4xl font-bold '>
-          <h1 className=''> STAKE </h1>
-        </div>
         <nav className='flex text-xl font-semibold ' >
           <Link className='px-10' to="/">HOME</Link>
           <Link to="/vault">CREATE VAULT</Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,6 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='sm:flex justify-between p-10 '>
-        <div className='md:text-3xl lg:text-4xl font-bold '>
-          <h1 className=''> CREATE A VAULT</h1>
-        </div>
         <nav className='flex text-xl font-medium ' >
           <Link className='px-10' to="/">HOME</Link>
           <Link to="/stake">STAKE</Link>
